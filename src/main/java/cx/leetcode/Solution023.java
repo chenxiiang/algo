bc45c06b30be2be427d246d2b7ae6ddcdbd91aa3
jiaowodashujiaowoemo <chenxiang950926@gmail.com>
2022-08-22 00:30:39 +0800

    双指针练习-023

---
+++ src/main/java/cx/leetcode/Solution023.java
@@ -0,0 +1,30 @@
+package cx.leetcode;
+
+public class Solution023 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA, p2 = headB;
+        while (p1 != p2) {
+            if (p1 != null) {
+                p1 = p1.next;
+            } else {
+                p1 = headB;
+            }
+            if (p2 != null) {
+                p2 = p2.next;
+            } else {
+                p2 = headA;
+            }
+        }
+        return p1;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
