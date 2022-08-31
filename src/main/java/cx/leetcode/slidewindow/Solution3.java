e83200580ece2128b4d0c09ae9be7587ca571a8d
chenxiang <cxiang1874@163.com>
2022-08-31 23:13:25 +0800

    滑动窗口

---
+++ src/main/java/cx/leetcode/slidewindow/Solution3.java
@@ -0,0 +1,28 @@
+package cx.leetcode.slidewindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution3 {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> window = new HashMap<>();
+        int left = 0, right = 0;
+        int res = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            window.put(c, window.getOrDefault(c, 0) + 1);
+            while (window.get(c) > 1) {
+                char d = s.charAt(left);
+                left++;
+                window.put(d, window.getOrDefault(d, 1) - 1);
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
+    }
+
+}
