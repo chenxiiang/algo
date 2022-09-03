381bc416b3e8521d50a73632053132485ea97f4b
chenxiang <cxiang1874@163.com>
2022-09-03 23:11:48 +0800

    二分查找

---
+++ src/main/java/cx/leetcode/binarysearch/Solution34.java
@@ -0,0 +1,47 @@
+package cx.leetcode.binarysearch;
+
+public class Solution34 {
+    public int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0 || target < nums[0] || target > nums[nums.length - 1]) {
+            return new int[]{-1, -1};
+        }
+        return new int[]{findLeft(nums, target), findRight(nums, target)};
+    }
+
+    //左右各一个方法可以求解，但是重复太多，能不能简化？
+    private int findLeft(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] == target) {
+                right = mid - 1;
+            }
+        }
+        if (left >= nums.length || nums[left] != target) {
+            return -1;
+        }
+        return left;
+    }
+
+    private int findRight(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] == target) {
+                left = mid + 1;
+            }
+        }
+        if (right < 0 || nums[right] != target) {
+            return -1;
+        }
+        return right;
+    }
+}
