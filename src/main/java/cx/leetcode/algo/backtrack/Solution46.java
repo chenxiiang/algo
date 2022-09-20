9e771d25083c9b4c056e51f063daec7450d0b330
chenxiang <cxiang1874@163.com>
2022-09-20 22:59:46 +0800

    全排列

---
+++ src/main/java/cx/leetcode/algo/backtrack/Solution46.java
@@ -0,0 +1,38 @@
+package cx.leetcode.algo.backtrack;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution46 {
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> permute(int[] nums) {
+        LinkedList<Integer> track = new LinkedList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, track, used);
+        return res;
+    }
+
+    // 路径：记录在 track 中
+    // 选择列表：nums 中不存在于 track 的那些元素（used[i] 为 false）
+    // 结束条件：nums 中的元素全都在 track 中出现
+    private void backtrack(int[] nums, LinkedList<Integer> track, boolean[] used) {
+        if (track.size() == nums.length) {
+            res.add(new LinkedList<>(track));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            //排除不合法的选择
+            if (used[i]) {
+                // nums[i]已经在track中，跳过
+                continue;
+            }
+            track.add(nums[i]);
+            used[i] = true;
+            backtrack(nums, track, used);
+            track.removeLast();
+            used[i] = false;
+        }
+    }
+}
