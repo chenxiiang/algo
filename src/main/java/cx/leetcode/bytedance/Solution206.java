3d6a099a7acb93e1ff32a64d5884883e6f7cb5a4
chenxiang <cxiang1874@163.com>
2022-10-16 20:14:46 +0800

    翻转链表

---
+++ src/main/java/cx/leetcode/bytedance/Solution206.java
@@ -0,0 +1,30 @@
+package cx.leetcode.bytedance;
+
+public class Solution206 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode last = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
