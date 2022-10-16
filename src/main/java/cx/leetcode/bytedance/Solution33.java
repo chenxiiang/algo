5062dedcf3aa0a079a96ecc2c41f7b48ec699792
chenxiang <cxiang1874@163.com>
2022-10-16 21:50:39 +0800

    33. 搜索旋转排序数组

---
+++ src/main/java/cx/leetcode/bytedance/Solution33.java
@@ -0,0 +1,36 @@
+package cx.leetcode.bytedance;
+
+public class Solution33 {
+    //将数组一分为二，其中一定有一个是有序的，另一个可能是有序，也能是部分有序。
+    //此时有序部分用二分法查找。无序部分再一分为二，其中一个一定有序，另一个可能有序，可能无序。就这样循环.
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
