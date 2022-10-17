e518940178c5150346a7a69a544c202d6c2f2f8a
chenxiang <cxiang1874@163.com>
2022-10-17 23:25:52 +0800

    121. 买卖股票的最佳时机-一次交易次数，无冷静期

---
+++ src/main/java/cx/leetcode/bytedance/Solution121.java
@@ -0,0 +1,33 @@
+package cx.leetcode.bytedance;
+
+public class Solution121 {
+    public int maxProfit1(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+
+    //动态规划
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        //第一维表示天数，第二维表示这一天是否持有
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+}
