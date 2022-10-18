8a918bc10fa0ac057c29e9096b5127198ed98fc1
chenxiang <cxiang1874@163.com>
2022-10-18 22:34:59 +0800

    103. 二叉树的锯齿形层序遍历

---
+++ src/main/java/cx/leetcode/bytedance/Solution103.java
@@ -0,0 +1,55 @@
+package cx.leetcode.bytedance;
+
+import java.util.*;
+
+public class Solution103 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        boolean fromLeft = false;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            Deque<Integer> valList = new ArrayDeque<>();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                if (fromLeft) {
+                    valList.offerFirst(curr.val);
+                } else {
+                    valList.offerLast(curr.val);
+                }
+                if (curr.left != null) {
+                    q.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    q.offer(curr.right);
+                }
+            }
+            res.add(new ArrayList<>(valList));
+            fromLeft = !fromLeft;
+        }
+        return res;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
