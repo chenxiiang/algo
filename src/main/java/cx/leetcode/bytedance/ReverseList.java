388f4c3cf337aa0df60159844501bab136cd8eda
chenxiang <cxiang1874@163.com>
2022-10-19 22:37:00 +0800

    翻转链表1

---
+++ src/main/java/cx/leetcode/bytedance/ReverseList.java
@@ -0,0 +1,22 @@
+package cx.leetcode.bytedance;
+
+public class ReverseList {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode last = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
