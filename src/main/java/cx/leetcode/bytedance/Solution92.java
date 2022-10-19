afb4a6b82b42faa124d7d7a39b5d0e6944f2d7cb
chenxiang <cxiang1874@163.com>
2022-10-19 22:24:14 +0800

    92. 反转链表 II

---
+++ src/main/java/cx/leetcode/bytedance/Solution92.java
@@ -0,0 +1,41 @@
+package cx.leetcode.bytedance;
+
+public class Solution92 {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == 1) {
+            return reverseN(head, right);
+        }
+        head.next = reverseBetween(head.next, left - 1, right - 1);
+        return head;
+    }
+
+    ListNode successor = null;
+
+    private ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            successor = head.next;
+            return head;
+        }
+        ListNode last = reverseN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor;
+        return last;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
