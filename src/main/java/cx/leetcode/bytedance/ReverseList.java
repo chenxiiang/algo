9fbd8a5b51a66c17479aa00ca6141184a19f33b3
chenxiang <cxiang1874@163.com>
2022-10-19 22:41:37 +0800

    翻转链表1

--- src/main/java/cx/leetcode/bytedance/ReverseList.java
+++ src/main/java/cx/leetcode/bytedance/ReverseList.java
@@ -2,6 +2,8 @@ package cx.leetcode.bytedance;
 
 public class ReverseList {
     public ListNode reverseList(ListNode head) {
+        //如果head就是个空链表，直接返回null
+        //如果到达最后一个节点，则该节点就是反转后的头结点
         if (head == null || head.next == null) {
             return head;
         }
