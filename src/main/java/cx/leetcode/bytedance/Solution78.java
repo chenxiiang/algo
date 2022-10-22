a6fb1b851695dae442a52493e7fb9e4bd4a84db6
chenxiang <cxiang1874@163.com>
2022-10-22 22:19:17 +0800

    78. 子集

---
+++ src/main/java/cx/leetcode/bytedance/Solution78.java
@@ -0,0 +1,24 @@
+package cx.leetcode.bytedance;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution78 {
+    List<List<Integer>> res = new ArrayList<>();
+    LinkedList<Integer> track = new LinkedList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        backtrack(nums, 0);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int start) {
+        res.add(new LinkedList<>(track));
+        for (int i = start; i < nums.length; i++) {
+            track.addLast(nums[i]);
+            backtrack(nums, i + 1);
+            track.removeLast();
+        }
+    }
+}
