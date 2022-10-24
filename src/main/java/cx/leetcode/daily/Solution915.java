a8a0b422d71b8c360b97ecea264d830387da6a52
chenxiiang <111284778+chenxiiang@users.noreply.github.com>
2022-10-24 15:26:24 +0800

    Create Solution915.java

---
+++ src/main/java/cx/leetcode/daily/Solution915.java
@@ -0,0 +1,21 @@
+package  cx.leetcode.daily;
+
+/**
+ * 数组分成左右两部分，左边的都小于右边所有的
+ */
+
+public class Solution915 {
+    public int partitionDisjoint(int[] nums) {
+        int n = nums.length;
+        int leftMax = nums[0], leftPos = 0, currMax = nums[0];
+        for (int i = 1; i < n - 1; i++) {
+            //0-i位置中的最大元素
+            currMax = Math.max(currMax, nums[i]);
+            if (nums[i] < leftMax) {
+                leftPos = i;
+                leftMax = currMax;
+            }
+        }
+        return leftPos + 1;
+    }
+}
