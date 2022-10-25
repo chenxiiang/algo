a137a2830aa23cfe2adcfc20416843aa23de3fa4
chenxiang <cxiang1874@163.com>
2022-10-25 23:34:12 +0800

    94. 二叉树的中序遍历

---
+++ src/main/java/cx/leetcode/bytedance/Solution94.java
@@ -0,0 +1,36 @@
+package cx.leetcode.bytedance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution94 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        list.addAll(inorderTraversal(root.left));
+        list.add(root.val);
+        list.addAll(inorderTraversal(root.right));
+        return list;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
