427bc33ebffb3e914513602251740be8d80e0f08
chenxiang <cxiang1874@163.com>
2022-10-26 22:36:31 +0800

    101. 对称二叉树

---
+++ src/main/java/cx/leetcode/bytedance/Solution101.java
@@ -0,0 +1,22 @@
+package cx.leetcode.bytedance;
+
+
+public class Solution101 {
+    //用迭代或者层序遍历+头尾指针
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return check(root.left, root.right);
+    }
+
+    private boolean check(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return check(left.left, right.right) && check(left.right, right.left);
+    }
+}
