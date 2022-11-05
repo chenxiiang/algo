975a5fae786e1996dd7277215da942160163a52a
chenxiiang <cxiang1874@163.com>
2022-11-05 23:59:20 +0800

    617. 合并二叉树

---
+++ src/main/java/cx/leetcode/bytedance/Solution617.java
@@ -0,0 +1,29 @@
+package cx.leetcode.bytedance;
+
+public class Solution617 {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        //会改变原二叉树
+//        if (root1 == null) {
+//            return root2;
+//        }
+//        if (root2 == null) {
+//            return root1;
+//        }
+//        root1.val += root2.val;
+//        root1.left = mergeTrees(root1.left, root2.left);
+//        root1.right = mergeTrees(root1.right, root2.right);
+//        return root1;
+
+        //不会改变原有二叉树
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        TreeNode root = new TreeNode(root1.val + root2.val);
+        root.left = mergeTrees(root1.left, root2.left);
+        root.right = mergeTrees(root1.right, root2.right);
+        return root;
+    }
+}
