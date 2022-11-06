5b6414e7108e726306d7002426d6eff6c780bcdb
chenxiiang <cxiang1874@163.com>
2022-11-06 23:41:42 +0800

    146. LRU 缓存

---
+++ src/main/java/cx/leetcode/bytedance/Solution146.java
@@ -0,0 +1,111 @@
+package cx.leetcode.bytedance;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution146 {
+    class Node {
+        Node prev, next;
+        int key, val;
+
+        public Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    class DoubleList {
+        private Node head, tail;
+        private int size;
+
+        public DoubleList() {
+            head = new Node(0, 0);
+            tail = new Node(0, 0);
+            head.next = tail;
+            tail.prev = head;
+            size = 0;
+        }
+
+        //把最近添加/访问的放到链表尾部，比较符合习惯
+        public void addLast(Node x) {
+            x.prev = tail.prev;
+            x.next = tail;
+            tail.prev.next = x;
+            tail.prev = x;
+            size++;
+        }
+
+        private void remove(Node x) {
+            x.prev.next = x.next;
+            x.next.prev = x.prev;
+            size--;
+        }
+
+        public Node removeFirst() {
+            if (head.next == tail) {
+                return null;
+            }
+            Node first = head.next;
+            remove(first);
+            return first;
+        }
+
+        public int size() {
+            return size;
+        }
+    }
+
+    private Map<Integer, Node> map;
+    private DoubleList cache;
+    private int cap;
+
+    public Solution146(int capacity) {
+        this.cap = capacity;
+        map = new HashMap<>();
+        cache = new DoubleList();
+    }
+
+    private void makeRecently(int key) {
+        Node x = map.get(key);
+        cache.remove(x);
+        cache.addLast(x);
+    }
+
+    private void addRecently(int key, int val) {
+        Node x = new Node(key, val);
+        cache.addLast(x);
+        map.put(key, x);
+    }
+
+    private void deleteKey(int key) {
+        Node x = map.get(key);
+        cache.remove(x);
+        map.remove(key);
+    }
+
+    private void removeLeastRecently() {
+        Node deletedNode = cache.removeFirst();
+        int deletedKey = deletedNode.key;
+        map.remove(deletedKey);
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        makeRecently(key);
+        return map.get(key).val;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            deleteKey(key);
+            addRecently(key, value);
+            return;
+        }
+        if (cap == cache.size) {
+            removeLeastRecently();
+        }
+        addRecently(key, value);
+    }
+}
