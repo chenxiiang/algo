942ec30f0cd01f329d1e0e25429966d67599d4eb
chenxiiang <cxiang1874@163.com>
2022-11-07 23:15:28 +0800

    42. 接雨水

--- src/main/java/cx/leetcode/bytedance/Solution42.java
+++ src/main/java/cx/leetcode/bytedance/Solution42.java
@@ -18,4 +18,29 @@ public class Solution42 {
         }
         return res;
     }
+
+    //上面的是简化后的写法，原始写法如下，也就是动态规划的双指针简化
+    public int trap1(int[] height) {
+        if (height.length == 0) {
+            return 0;
+        }
+        int n = height.length;
+        int res = 0;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        leftMax[0] = height[0];
+        rightMax[n - 1] = height[n - 1];
+        //两个数组和下面的三次循环可以简化为上面的while循环
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+
+        for (int i = 1; i < n; i++) {
+            res += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return res;
+    }
 }
