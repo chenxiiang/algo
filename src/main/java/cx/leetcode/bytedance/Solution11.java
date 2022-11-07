d7cdb4d3a75537bad07c1cce5b3beb8c81cc1480
chenxiiang <cxiang1874@163.com>
2022-11-07 23:19:46 +0800

    11. 盛最多水的容器

--- src/main/java/cx/leetcode/bytedance/Solution11.java
+++ src/main/java/cx/leetcode/bytedance/Solution11.java
@@ -7,6 +7,7 @@ public class Solution11 {
         while (left < right) {
             int currArea = Math.min(height[left], height[right]) * (right - left);
             res = Math.max(res, currArea);
+            //这里的指针是大的那个不动，小的向中间移动，因为要求最大值的情况下，而矩形的面积是由较低的两条边决定的，也就是木桶理论，如果不把短的补高，那么高的再高，木桶能装的水也还是那么多
             if (height[left] < height[right]) {
                 left++;
             } else {
