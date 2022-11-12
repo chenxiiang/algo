787417844cc66efa9a8d93b446a4d0990ab77fac
chenxiiang <cxiang1874@163.com>
2022-11-12 22:56:48 +0800

    12. 整数转罗马数字

---
+++ src/main/java/cx/leetcode/bytedance/Solution12.java
@@ -0,0 +1,16 @@
+package cx.leetcode.bytedance;
+
+public class Solution12 {
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] reps = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String res = "";
+        for (int i = 0; i < 13; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                res = res + reps[i];
+            }
+        }
+        return res;
+    }
+}
